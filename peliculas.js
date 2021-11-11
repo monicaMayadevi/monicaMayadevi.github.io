dac2e061e41c9d3e119cf8dced888bb664a94c47
Monica Silva Lopez <msilva@correo.ler.uam.mx>
2021-11-11 12:36:49 -0600

    listado-peliculas

--- peliculas.js
+++
@@ -1,1 +0,0 @@
-[{"nombre":"Viernes 13","genero":"Terror","clasificacion":"B","img":"../../img/peli1.png"},{"nombre":"Bambi","genero":"Infantil","clasificacion":"A","img":"../../img/peli2.png"},{"nombre":"Rambo","genero":"Accion","clasificacion":"B","img":"../../img/peli1.png"},{"nombre":"XXX","genero":"Accion","clasificacion":"C","img":"../../img/peli2.png"}]
